51f6b615def8336671cb3228d097dee446b58661
Nurizki <nutrisi1udin@gmail.com>
2022-10-01 09:30:38 +0700

    tugas 4 php
    tugas niomic

---
+++ tugas4_1.php
@@ -0,0 +1,10 @@
+<?php
+$beli = 8000;
+$jual = 75000;
+$untung = 3000;
+echo "Harga Beli = Rp $beli<br/>";
+echo "Harga Jual = Rp $jual<br/>";
+echo "Keuntungan = Rp $untung<br/>";
+$jumlah = ($jual-$untung)/$beli;
+echo "Jumlah kambing yang dibeli = $jumlah ekor";
+?>
---
+++ tugas4_2.php
@@ -0,0 +1,8 @@
+<?php
+$saluran = 523;
+$terpakai = 8891;
+echo "Jumlah Saluran = $saluran cabang<br/>";
+echo "Jumlah Terpakai = $terpakai liter<br/>";
+$rata2 = $terpakai/$saluran;
+echo "Rata-rata Pemakaian = $rata2 liter/keluarga<br/>";
+?>
